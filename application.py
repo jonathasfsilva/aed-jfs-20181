59bd5405be2285233a4a3f2052092cf6e7dedbe1
Jonathas F. Silva <jonathas_ssilva@hotmail.com>
2019-06-21 09:18:13 -0300

    interface close #6

--- application.py
+++ application.py
@@ -1,1 +1,36 @@
-## TODO: create application
+import os
+
+os.system('clear')
+while True:
+    opcao = int(input("Títulos Cadastrados: %d" % quantidade_cadastrada+'\n\n' +
+                      "1 - Cadastrar título" + '\n' +
+                      "2 - Descadastrar título" + '\n' +
+                      "3 - Carregar de um arquivo" + '\n' +
+                      "4 - Exibir títulos cadastrados" + '\n' + '\n'
+                      "5 - Finaliza cadastro de títulos" + '\n' + '\n'
+                      "Escolha: "))
+    if opcao == 1:
+        ## TODO: cadastrar
+        """
+        inserir na arvore vermelho e preto
+        """
+    elif opcao == 2:
+        ## TODO: descadastrar
+        """
+        remover na arvore vermelho e preto
+        """
+    elif opcao == 3:
+        ## TODO: carregar
+        """
+        inserir atraves de um Arquivo
+        """
+    elif opcao == 4:
+        ## TODO: exibir
+        """
+        lista em ordem
+        """
+    elif opcao == 5:
+        ## TODO: finalizar
+        """
+        passar pra proxima etapa: candidatos
+        """
