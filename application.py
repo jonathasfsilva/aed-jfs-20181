a209a3ec24fbe7bd5dc1199bfd86d437c24034bd
Jonathas F. Silva <jonathas_ssilva@hotmail.com>
2019-06-24 18:53:03 -0300

    descadastrar close #8

--- application.py
+++ application.py
@@ -20,10 +20,18 @@ while True:
         titulos.insert(titulo.Titulo.cadastrar())
         quantidade_cadastrada += 1
     elif opcao == 2:
-        ## TODO: descadastrar
-        """
-        remover na arvore vermelho e preto
-        """
+        os.system('clear')
+        if not titulos.isEmpty():
+            eliminar = titulos.search(titulo.Titulo.descadastrar())
+            # quando um ítem não é encontrado a função search retorna 'nil'
+            # para não apagar a folha é feita a verificação abaixo
+            if eliminar is not titulos.nil:
+                titulos.delete(eliminar)
+                quantidade_cadastrada -= 1
+            else:
+                print('Titulo nao encontrado!')
+        else:
+            print('Árvore vazia!')
     elif opcao == 3:
         ## TODO: carregar
         """
